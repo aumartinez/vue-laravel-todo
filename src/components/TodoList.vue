785cbb1e853afbc3fb235294d467fa626426f56a
aumartines <acm_asd@hotmail.com>
2022-09-23 17:17:06 -0600

    Setting up ToDo List

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,15 +2,66 @@
   <div class="todolist">
     <div class="container">
       <div class="row">
-        <div class="col-md-12 pt-5 pb-5">
+        <div class="offset-md-3 col-md-6 pt-5 pb-5">
           <h1 class="text-center">
-            Lista de tareas
+            {{listTitle.value}}
+            <span>
+              <font-awesome-icon icon="fa-solid fa-clipboard" />
+            </span>
           </h1>
 
           <div class="list-wrapper">
-            <ul class="todo-container">
+            <div class="btns-wrapper">
+              <button class="btn bg-danger" type="button">
+                <span>
+                  Borrar todo
+                </span>
+                <font-awesome-icon icon="fa-solid fa-trash" />
+              </button>
 
+              <button class="btn bg-warning" type="button">
+                <span>
+                  Borrar seleccionadas
+                </span>
+                <font-awesome-icon icon="fa-solid fa-trash" />
+              </button>
+            </div>
+            <ul class="list-unstyled todo-container">
+              <li 
+              v-for="(task, index) in tasks.value"
+              :key="index">
+                <div class="form-check">
+                  <input class="form-check-input" 
+                  type="checkbox" 
+                  value="" 
+                  :id="`item-${index}`" />                  
+                  <label class="form-check-label" :for="`item-${index}`">
+                    {{task}}
+                  </label>
+                </div>
+              </li>
             </ul>
+            <div class="input-group pt-3 pb-3">
+              <input type="text" 
+              class="form-control" 
+              placeholder="Agregua una tarea" 
+              aria-label="Agregar tarea" 
+              aria-describedby="button-add" />
+              <button 
+              class="btn btn-outline-secondary btn-primary" 
+              type="button" 
+              id="button-add">
+                <font-awesome-icon icon="fa-solid fa-plus" />
+              </button>
+            </div>
+            <div class="list-bottom">
+              <button class="btn bg-success" type="button">
+                <span>
+                  Guardar
+                </span>
+                <font-awesome-icon icon="fa-solid fa-cloud-arrow-up" />
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -21,8 +72,19 @@
 <script>
 export default {
   name: 'TodoList',
-  props: {
-    msg: String
+  data() {
+    return {
+      tasks: {
+        type: Array,
+        required: false,
+        value: [1, 2, 3]
+      },
+      listTitle: {
+        type: String,
+        required: true,
+        value: 'Lista de tareas'
+      }
+    }
   }
 }
 </script>
@@ -31,5 +93,17 @@ export default {
 <style scoped>
   .todolist {
     margin: 5rem 0;
+  }
+
+  .btns-wrapper,
+  .list-bottom {
+    display:flex;
+    flex-direction: row;
+    justify-content: flex-end;
+  }
+
+  button {
+    color: #fff;
+    margin-right: 1rem;
   }
 </style>
