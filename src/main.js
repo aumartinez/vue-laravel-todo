3d6ea5b29acbaf1fd8a0781e46e1ee0f37569a82
aumartines <acm_asd@hotmail.com>
2022-09-23 14:59:45 -0600

    Initial setup

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+/* import Bootstrap */
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+library.add(fas, far, fab)
+
+const app = createApp(App)
+app.use(createPinia())
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.mount('#app')
