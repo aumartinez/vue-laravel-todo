60c395edf384a0bc8dcc85b5edea0642eeedb7b1
aumartines <acm_asd@hotmail.com>
2022-09-25 18:20:25 -0600

    Integrating Vue build files to Laravel

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
+const path = require('path')
+
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir: path.resolve(__dirname, './laravel/public'),
+  filenameHashing: false
 })
